484140bf647734c6c0aac5ecdb847a540d0e31b4
Noe <noe.satorres@gmail.com>
2024-02-13 23:10:31 -0300

    cree listar y buscar correctamente

--- appp.py
+++ appp.py
@@ -5,17 +5,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/electrodomesticos'
 db = SQLAlchemy(app)
 
+
 class Electro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30), nullable=False)
     modelo = db.Column(db.String(30), unique=True, nullable=False)
     cantidad = db.Column(db.Integer, nullable=False)
-    precio = db.Column(db.Float, nullable=False)  
+    precio = db.Column(db.Float, nullable=False)
 
     def __init__(self, nombre, modelo, cantidad, precio):
         self.nombre = nombre
         self.modelo = modelo
-        self.cantidad = cantidad  
+        self.cantidad = cantidad
         self.precio = precio
 
 
@@ -32,6 +33,31 @@ def agregar_electrodomestico():
     db.session.add(nuevo_electrodomestico)
     db.session.commit()
     return jsonify({'message': 'Se agregó un nuevo electrodomestico'}), 200
+
+# LISTAR
+
+
+@app.route('/electrodomesticos', methods=['GET'])
+def obtener_electrodomestico():
+    electrodomesticos = Electro.query.all()
+    electrodomesticos_json = [{'nombre': electrodomestico.nombre, 'modelo': electrodomestico.modelo,
+                              'cantidad': electrodomestico.cantidad, 'precio': electrodomestico.precio} for electrodomestico in electrodomesticos]
+    return jsonify(electrodomesticos_json),200
+
+#BUSCAR
+@app.route('/electrodomesticos/buscar', methods=['POST'])
+def buscar_electrodomestico():
+    data = request.get_json()
+    modelo = data.get('modelo')
+    if modelo:
+        electrodomestico = Electro.query.filter_by(modelo=modelo).first()
+        if electrodomestico:
+            electrodomesticos_json = [{'nombre': electrodomestico.nombre, 'modelo': electrodomestico.modelo,
+                              'cantidad': electrodomestico.cantidad, 'precio': electrodomestico.precio}]
+            return jsonify (electrodomesticos_json), 200
+        return jsonify ({'message': 'Electrodomestico no encontrado'}),404
+            
+
 
 if __name__ == '__main__':
     with app.app_context():
